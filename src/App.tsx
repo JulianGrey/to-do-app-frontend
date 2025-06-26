de92a898be05d0b9342b247a6566310301038a50
Julian Grey <julianrgrey@gmail.com>
2025-06-26 17:06:52 +0100

    Create ToDo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { useState } from 'react';
 import './App.scss';
+import { ToDo } from './components/ToDo/ToDo';
+
+
 
 function App() {
   const defaultMessage = 'You have no tasks.';
   const headerTitle = 'Your To-dos';
-  const [toDoList, setToDoList] = useState([]);
+  const [toDoList, setToDoList] = useState([
+    {
+      category: 'Event',
+      description: 'To Do Description',
+      title: 'To Do Title',
+    },
+    {
+      title: 'To Do Title 2',
+    },
+    {
+      description: 'To Do Description 3',
+      title: 'To Do Title 3',
+    },
+  ]);
 
   return (
     <div className='to-do-app'>
@@ -12,11 +28,11 @@ function App() {
         <h1>{headerTitle}</h1>
       </header>
       <main>
-        <div>
+        <div className='to-do-list'>
           {
             toDoList.length > 0 && (
               <ul>
-                {toDoList.map((toDo, index) => <li key={index}>{toDo}</li>)}
+                {toDoList.map((toDo, index) => <ToDo toDo={toDo} key={index} />)}
               </ul>
             )
           }
---
+++ src/components/ToDo/ToDo.tsx
@@ -0,0 +1,19 @@
+import './ToDo.scss';
+
+interface ToDoProps {
+  category?: string,
+  description?: string,
+  title: string,
+}
+
+export function ToDo({ toDo }: { toDo: ToDoProps }) {
+  return (
+    <li className='to-do'>
+      <div className='to-do-headline'>
+        <h2 className='to-do-title'>{toDo.title}</h2>
+        {toDo.category && <h3 className='to-do-category'>({toDo.category})</h3>}
+      </div>
+      {toDo.description && <h4 className='to-do-description'>{toDo.description}</h4>}
+    </li>
+  );
+}
