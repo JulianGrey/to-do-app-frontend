0fd07276621bb0e07d3b9cafbd4ccd84d316278b
Julian Grey <julianrgrey@gmail.com>
2025-06-26 13:08:44 +0100

    Add linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import stylistic from '@stylistic/eslint-plugin'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
@@ -19,5 +20,14 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/eol-last': ['error'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/no-trailing-spaces': ['error'],
+      '@stylistic/semi': ['error', 'always']
+    }
   },
 ])
