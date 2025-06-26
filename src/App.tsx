ce296f271b85915982e64417ae42140a2d686968
Julian Grey <julianrgrey@gmail.com>
2025-06-26 14:45:12 +0100

    Set up initial to-do page logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const defaultMessage = 'You have no tasks.';
+  const headerTitle = 'Your To-dos';
+  const [toDoList, setToDoList] = useState([]);
+
   return (
-    <div></div>
+    <div className='to-do-app'>
+      <header>
+        <h1>{headerTitle}</h1>
+      </header>
+      <main>
+        <div>
+          {
+            toDoList.length > 0 && (
+              <ul>
+                {toDoList.map((toDo, index) => <li key={index}>{toDo}</li>)}
+              </ul>
+            )
+          }
+          {
+            !toDoList.length && (<p>{defaultMessage}</p>)
+          }
+        </div>
+      </main>
+      <footer></footer>
+    </div>
   );
 }
 
