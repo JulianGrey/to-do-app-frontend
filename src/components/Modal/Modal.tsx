499ce92e27c3903c8212e4c778f5dcce065e717c
Julian Grey <julianrgrey@gmail.com>
2025-07-09 23:35:03 +0100

    Implement deletion modal
    Basic UI modal which acts as a confirmation dialog allowing the user to finalise or cancel the deletion of a todo.

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+import './Modal.scss';
+
+interface ModalProps {
+  onCancel: () => void;
+  onConfirm: () => void;
+  children?: ReactNode;
+}
+
+export default function Modal({ onCancel, onConfirm, children }: ModalProps) {
+  return (
+    <div className='modal'>
+      <div className='modal-dialog'>
+        { children }
+        <div className='modal-actions'>
+          <button onClick={onCancel}>Cancel</button>
+          <button onClick={onConfirm}>Confirm</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import './Todo.scss';
+import Modal from '../Modal/Modal';
 
 export interface TodoProps {
   title: string,
@@ -27,13 +29,14 @@ export default function Todo({
   const [editedDescription, setEditedDescription] = useState('');
   const [editedTitle, setEditedTitle] = useState('');
   const [isEditing, setIsEditing] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     setCurrentTitle(todo.title);
     setCurrentDescription(todo.description);
   }, [todo.title, todo.description]);
 
-  function handleEditTtile(event: React.ChangeEvent<HTMLInputElement>) {
+  function handleEditTitle(event: React.ChangeEvent<HTMLInputElement>) {
     setEditedTitle(event.target.value);
   }
 
@@ -43,6 +46,17 @@ export default function Todo({
       setEditedTitle('');
       setEditedDescription('');
     }
+  }
+
+  async function handleDelete() {
+    if (todo.id !== undefined) {
+      await onDelete(todo.id);
+      setShowModal(false);
+    }
+  }
+
+  function handleShowModal(value: boolean) {
+    setShowModal(value);
   }
 
   function handleIsEditing(value: boolean) {
@@ -61,91 +75,97 @@ export default function Todo({
   }
 
   return (
-    <li className='to-do'>
-      <div className='to-do-header'>
-        <div className='headline'>
-          { isNewTodo || isEditing
-            ? (
-              <div className='edit-to-do-title edit-to-do'>
-                <label htmlFor='edit-to-do-title'>Title:</label>
-                <input
-                  id='edit-to-do-title'
-                  type='text'
-                  maxLength={40}
-                  onChange={handleEditTtile}
-                  placeholder='40 character limit'
-                  value={editedTitle} />
+    <>
+      <li className='to-do'>
+        <div className='to-do-header'>
+          <div className='headline'>
+            { isNewTodo || isEditing
+              ? (
+                <div className='edit-to-do-title edit-to-do'>
+                  <label htmlFor='edit-to-do-title'>Title:</label>
+                  <input
+                    id='edit-to-do-title'
+                    type='text'
+                    maxLength={40}
+                    onChange={handleEditTitle}
+                    placeholder='40 character limit'
+                    value={editedTitle} />
+                </div>
+              )
+              : (<h2 className='to-do-title' data-testid='to-do-title'>{currentTitle}</h2>)
+            }
+          </div>
+          <div className='actions'>
+            {
+              (!isEditing && !isNewTodo) && (
+                <>
+                  <button
+                    className='edit'
+                    data-testid='edit-button'
+                    onClick={() => handleIsEditing(true)}
+                  >Edit</button>
+                  <button
+                    className='delete'
+                    onClick={() => handleShowModal(true)}
+                    data-testid='delete-button'
+                  >Delete</button>
+                </>
+              )
+            }
+            {
+              isEditing && (
+                <>
+                  <button
+                    className='update'
+                    onClick={handleUpdate}
+                    data-testid='update-button'
+                  >Update</button>
+                  <button
+                    className='cancel'
+                    onClick={() => handleIsEditing(false)}
+                    data-testid='cancel-button'
+                  >Cancel</button>
+                </>
+              )
+            }
+            {
+              isNewTodo && (
+                <>
+                  <button
+                    className='add'
+                    onClick={handleAdd}
+                    data-testid='add-button'
+                  >Add new to do</button>
+                </>
+              )
+            }
+          </div>
+        </div>
+        <div className='to-do-body'>
+          { (isNewTodo || isEditing) &&
+            (
+              <div className='edit-to-do-description edit-to-do'>
+                <label htmlFor='edit-to-do-description'>Description:</label>
+                <textarea
+                  id='edit-to-do-description'
+                  rows={3}
+                  onChange={(event) => setEditedDescription(event.target.value)}
+                  value={editedDescription}
+                ></textarea>
               </div>
             )
-            : (<h2 className='to-do-title' data-testid='to-do-title'>{currentTitle}</h2>)
+          }
+          {currentDescription && !(isNewTodo || isEditing) &&
+            <p className='to-do-description' data-testid='to-do-description'>{currentDescription}</p>
           }
         </div>
-        <div className='actions'>
-          {
-            (!isEditing && !isNewTodo) && (
-              <>
-                <button
-                  className='edit'
-                  data-testid='edit-button'
-                  onClick={() => handleIsEditing(true)}
-                >Edit</button>
-                <button
-                  className='delete'
-                  onClick={() => {
-                    if (todo.id !== undefined) onDelete(todo.id);
-                  }}
-                  data-testid='delete-button'
-                >Delete</button>
-              </>
-            )
-          }
-          {
-            isEditing && (
-              <>
-                <button
-                  className='update'
-                  onClick={handleUpdate}
-                  data-testid='update-button'
-                >Update</button>
-                <button
-                  className='cancel'
-                  onClick={() => handleIsEditing(false)}
-                  data-testid='cancel-button'
-                >Cancel</button>
-              </>
-            )
-          }
-          {
-            isNewTodo && (
-              <>
-                <button
-                  className='add'
-                  onClick={handleAdd}
-                  data-testid='add-button'
-                >Add new to do</button>
-              </>
-            )
-          }
-        </div>
-      </div>
-      <div className='to-do-body'>
-        { (isNewTodo || isEditing) &&
-          (
-            <div className='edit-to-do-description edit-to-do'>
-              <label htmlFor='edit-to-do-description'>Description:</label>
-              <textarea
-                id='edit-to-do-description'
-                rows={3}
-                onChange={(event) => setEditedDescription(event.target.value)}
-                value={editedDescription}
-              ></textarea>
-            </div>
-          )
-        }
-        {currentDescription && !(isNewTodo || isEditing) &&
-          <p className='to-do-description' data-testid='to-do-description'>{currentDescription}</p>
-        }
-      </div>
-    </li>
+      </li>
+      {showModal && createPortal(
+        <Modal onCancel={() => setShowModal(false)} onConfirm={handleDelete}>
+          <p>Are you sure you want to delete "{todo.title}"?</p>
+        </Modal>,
+        document.body,
+      )}
+    </>
   );
 }
